b68574ba31449f1b1c7551c227f90c01f71c0153
Ian Gabaraev <ian@kuver.tech>
2020-10-13 22:59:19 +0300

    more comments

--- chevrons/tests.py
+++ chevrons/tests.py
@@ -62,7 +62,7 @@ class TestChevronize(unittest.TestCase):
     def test_unquote_regular_string_with_single_quote_unpaired(self):
         """
         Testing on regular string with single
-        substring with single quotes around
+        substring with unpaired single quote
 
         """
         original_string = "This is 'quoted"
@@ -73,7 +73,7 @@ class TestChevronize(unittest.TestCase):
     def test_unquote_regular_string_with_double_quote_unpaired(self):
         """
         Testing on regular string with single
-        substring with single quotes around
+        substring with unpaired double quote
 
         """
         original_string = 'This is "quoted'
@@ -83,9 +83,9 @@ class TestChevronize(unittest.TestCase):
 
     def test_unquote_and_chevronize_mixed_string(self):
         """
-        Testing on regular string with single
+        Testing on regular string with a
         substring with double quotes around
-        and single substring with unpaired double
+        and another substring with unpaired double
         quote
 
         """
@@ -96,9 +96,9 @@ class TestChevronize(unittest.TestCase):
 
     def test_unquote_and_chevronize_another_mixed_string(self):
         """
-        Testing on regular string with single
+        Testing on regular string with a
         substring with single quotes around
-        and single substring with unpaired single
+        and another single substring with unpaired single
         quote
 
         """
